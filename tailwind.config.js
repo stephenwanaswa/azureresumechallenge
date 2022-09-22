066588db7607b69c7b99d681dfc461eb09b4d511
stephenwanaswa <stephenwanaswa@gmail.com>
2022-09-22 15:26:04 +0300

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        body: "#17171F",
+        "selected-text": "#A3A3FF",
+        theme: "#3f3fff",
+        nav: "#404053",
+        secondary: "#9191a4",
+        badge: "#3f3f51",
+        "input-border": "#565666",
+        input: "#2a2a35",
+      },
+      fontFamily: {
+        poppins: ["'Poppins'", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
